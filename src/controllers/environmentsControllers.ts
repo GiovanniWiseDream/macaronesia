4970294f8bdf131cc37c8da18c4d7dbe866e552c
etielcll <135212306+etielcll@users.noreply.github.com>
2023-10-18 14:10:32 +0100

    Controlador que evita nombre duplicado al crear

--- src/controllers/environmentsControllers.ts
+++ src/controllers/environmentsControllers.ts
@@ -3,9 +3,18 @@ const mongoose = require("mongoose");
 import { Request, Response, RequestHandler } from "express";
 
 export class environmentsController {
+
   static async CreateEnvironment(req, res) {
     const { name, image, map, environments } = req.body;
     try {
+      // Verifica si ya existe un ambiente con el mismo nombre
+      const existingEnvironment = await Environments.findOne({ name });
+  
+      if (existingEnvironment) {
+        // Si ya existe, responde con un código de estado 400 (Bad Request)
+        return res.status(400).json({ message: "Ya existe un entorno con ese nombre" });
+      }
+  
       // Crea una nueva instancia del modelo con los datos proporcionados
       const newEnvironment = new Environments({
         name,
@@ -13,9 +22,10 @@ export class environmentsController {
         map,
         image,
       });
-
+  
       // Guarda el nuevo ambiente en la base de datos
       await newEnvironment.save();
+  
       // Responde con el ambiente creado y un código de estado 201 (creado)
       res.status(201).json(newEnvironment);
     } catch (error) {
@@ -24,6 +34,8 @@ export class environmentsController {
       res.status(500).json({ message: "Error interno del servidor" });
     }
   }
+
+
   static async GetEnvironment(req, res) {
     try {
       const environments = await Environments.find({}).select("name image map");
