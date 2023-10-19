4bdfd90f9fa3474479c229d4d2d9e4468af71d1f
Jose Daniel <jose_daniel@thewisedreams.com>
2023-10-19 16:33:37 +0100

    test 5 new config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Server } from "./server";
 
 let server = new Server().app;
-let port = parseInt(process.env.PORT) || 3000;
-const hostname = "127.0.0.1";
+let port = parseInt(process.env.PORT) || 3001;
+const hostname = process.env.HOST_NAME || "127.0.0.1";
 
 server.listen(port, hostname, () => {
   console.log(`Server is running at port ${port}`);
+  console.log(`Host Name: ${hostname}`) //Only For Dev, deleted in Production
 });
