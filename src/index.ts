50a7d4ebc6453f8e74514bdde394d56cfaae62b6
GiovanniWiseDream <giovanni@thewisedreams.com>
2023-10-20 11:28:53 +0100

    prueba

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Server } from "./server";
-
+import { keyvault } from "./keyvault";
 let server = new Server().app;
 let port = parseInt(process.env.PORT) || 3001;
 const hostname = process.env.HOST_NAME || "127.0.0.1";
-if (process.env) {
-  console.log(process.env);
+let secret = new keyvault();
+
+if (secret) {
+  console.log(secret);
 }
 server.listen(port, hostname, () => {
   console.log(`Server is running at port ${port}`);
-  console.log(`Host Name: ${hostname}`) //Only For Dev, deleted in Production
+  console.log(`Host Name: ${hostname}`); //Only For Dev, deleted in Production
 });
