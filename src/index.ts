624e4cc6303f6537ee977cf7f3fdfa09829a95a1
GiovanniWiseDream <giovanni@thewisedreams.com>
2023-10-20 10:10:42 +0100

    prueba variables

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Server } from "./server";
 let server = new Server().app;
 let port = parseInt(process.env.PORT) || 3001;
 const hostname = process.env.HOST_NAME || "127.0.0.1";
-
+if (process.env) {
+  console.log(process.env);
+}
 server.listen(port, hostname, () => {
   console.log(`Server is running at port ${port}`);
   console.log(`Host Name: ${hostname}`) //Only For Dev, deleted in Production
