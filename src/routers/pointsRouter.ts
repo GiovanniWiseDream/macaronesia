7167e0dbdc84c99e9c8ddb72798158738c18b8ed
pabloalemanr <pablo_aleman@canariaslivinglab.com>
2023-10-23 15:33:35 +0100

    pointsRouter

---
+++ src/routers/pointsRouter.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { pointsController } from "../controllers/pointController";
+import { format } from "path";
+class pointsRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.getRoutes();
+    this.postRoutes();
+    this.patchRoutes();
+
+    this.deleteRoutes();
+  }
+  getRoutes() {
+    this.router.post("/allPoints", pointsController.findAll);
+  }
+
+  deleteRoutes() {}
+  postRoutes() {
+    this.router.post("/getpoint", pointsController.findById);
+    this.router.post("/addPoints", pointsController.addPoints);
+  }
+
+  patchRoutes() {
+    format;
+    this.router.patch("/updatePlace", pointsController.deleteById);
+  }
+}
+export default new pointsRouter().router;
